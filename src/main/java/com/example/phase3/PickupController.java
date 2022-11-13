8f8630386623019814f589586c9c41f8430258d1
benja <bpham1618@gmail.com>
2022-11-12 17:40:59 -0700

    Pickup time set

--- src/main/java/com/example/phase3/PickupController.java
+++ src/main/java/com/example/phase3/PickupController.java
@@ -1,19 +1,33 @@
 package com.example.phase3;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.Node;
+import javafx.scene.control.ComboBox;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
-public class PickupController {
+public class PickupController{
     private Stage stage;
     private Scene scene;
     private Parent root;
 
+    @FXML
+    private ComboBox timeBox;
+    @FXML
+    private Text errorTime;
+    boolean cont = false;
+    ObservableList<String> time = FXCollections.observableArrayList("0000", "0100", "0200", "0300", "0400", "0500", "0600", "0700", "0800", "0900", "1100", "1200", "1300", "1400", "1500", "1600", "1700", "1800", "1900", "2000", "2100", "2200", "2300");
     public Customer customer;
     public Pizza pizza;
 
@@ -31,6 +45,10 @@ public class PickupController {
         //once this step is reached, pizza should THEN be added to order
         if(pizza != null) {
             customer.getOrder().getCart().add(pizza);
+        }
+        if(!cont) {
+            errorTime.setText("Please select a pickup time");
+            return;
         }
 
         //now, only customer need be passed moving forward
@@ -66,5 +84,18 @@ public class PickupController {
         scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
+    }
+
+    @FXML
+    public void initialize() {
+        timeBox.getItems().removeAll(timeBox.getItems());
+        timeBox.getItems().addAll(time);
+    }
+    public void seeTime(ActionEvent event) {
+        Object temp = timeBox.getValue();
+        String selected = temp.toString();
+        customer.getOrder().setPickupTime(selected);
+        System.out.println(customer.getOrder().getPickupTime());
+        cont = true;
     }
 }
