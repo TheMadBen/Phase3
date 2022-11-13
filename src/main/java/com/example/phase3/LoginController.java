5a02f3a0cdbe8bc33c602273cfa9791303ec1947
stombya <astomber@asu.edu>
2022-11-12 19:31:37 -0700

    Login verification email

--- src/main/java/com/example/phase3/LoginController.java
+++ src/main/java/com/example/phase3/LoginController.java
@@ -1,12 +1,16 @@
 package com.example.phase3;
 
 import java.io.IOException;
+import java.util.regex.Pattern;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.Node;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class LoginController {
@@ -14,8 +18,40 @@ public class LoginController {
     private Scene scene;
     private Parent root;
 
+    @FXML
+    private TextField userField;
+    @FXML
+    private TextField passField;
+
+    @FXML
+    private Text errMsg;
+
     public void switchToIncoming(ActionEvent event)  throws IOException{
         System.out.println(Store.testVar);
+        userField.getText();
+        passField.getText();
+
+        boolean proceed = true;
+
+
+
+
+
+        //if(!user)
+       if(!(userField.getText().contains("@") && userField.getText().contains(".com") )) {
+               System.out.println("fd");
+               errMsg.setText("Please enter a valid email");
+
+
+               return;
+       }
+
+
+
+
+
+
+
 
         Parent root = FXMLLoader.load(getClass().getResource("Incoming.fxml"));
         stage = (Stage)((Node)event.getSource()).getScene().getWindow();
